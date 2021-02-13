52ca74ef9551e43f376251578796c653eefa3f8f
Dylan Wesenberg <dylan.wesenberg@gmail.com>
2021-02-12 21:44:35 -0600

    Adding https

---
+++ README.md
@@ -0,0 +1,16 @@
+# Node + Docker Hello World
+
+A little hello world program to prove to myself that I can get a server up and running. Most of it is from this [tutorial](https://www.digitalocean.com/community/tutorials/how-to-build-a-node-js-application-with-docker).
+
+Docker build command:
+
+```
+docker build -t dylanwesenberg/nodejs-demo .
+```
+
+Docker run command:
+Replace \</host/sslKeyDirectory\> with host path to a folder containing ssl certs (privKey.pem and fullchain.pem)
+
+```
+docker run --name nodejs-image-demo -d -v </host/sslKeyDirectory>:/ssl -p 8080:8080 -p 8443:8443 dylanwesenberg/nodejs-image-demo
+```
--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
 const app = express();
 const router = express.Router();
 
-const path = __dirname + '/views/';
+const privateKey = fs.readFileSync("/ssl/privkey.pem");
+const certificate = fs.readFileSync("/ssl/fullchain.pem");
+
+const path = __dirname + "/views/";
 const port = 8080;
 
 router.use(function (req, res, next) {
-    console.log('/' + req.method);
-    next();
+  console.log("/" + req.method);
+  next();
 });
 
-router.get('/', function (req, res) {
-    res.sendFile(path + 'index.html');
+router.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
 });
 
-router.get('/sharks', function (req, res) {
-    res.sendFile(path + 'sharks.html');
+router.get("/sharks", function (req, res) {
+  res.sendFile(path + "sharks.html");
 });
 
 app.use(express.static(path));
-app.use('/', router);
+app.use("/", router);
 
-app.listen(port, function () {
-    console.log('Example app listening on port 8080!')
-})
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(
+  { key: privateKey, cert: certificate },
+  app
+);
+
+httpServer.listen(8080, function () {
+  console.info("Listening on 8080!");
+});
+httpsServer.listen(8443, function () {
+  console.info("Listening on 8443!");
+});
